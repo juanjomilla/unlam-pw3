d10cfc827c2f088debf7156144ac9f31b042fbaf
Juan Milla <juan.milla@southworks.com>
2020-05-17 20:58:06 -0300

    se agrega autofac como inyector de dependencias

---
+++ codigo/AyudandoEnLaPandemia/AyudandoEnLaPandemia/AutofacModules/DIModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace AyudandoEnLaPandemia.AutofacModules
+{
+    public class DIModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
