8033041f28f3ef0e2aa8ccc27353dee0e449df4e
rodrigososa21 <rodriisosa21@gmail.com>
2020-07-05 00:54:26 -0300

    Detalles previos a la entrega

---
+++ scripts_sql/20201c-ScriptInsertMotivoDenuncia.sql
@@ -0,0 +1,30 @@
+USE [TP-20201C]
+GO
+
+INSERT INTO [dbo].[MotivoDenuncia]
+           ([IdMotivoDenuncia]
+           ,[Descripcion])
+     VALUES
+           (1, 'Fraude')
+GO
+
+INSERT INTO [dbo].[MotivoDenuncia]
+           ([IdMotivoDenuncia]
+           ,[Descripcion])
+     VALUES
+           (2, 'Indebida')
+GO
+
+INSERT INTO [dbo].[MotivoDenuncia]
+           ([IdMotivoDenuncia]
+           ,[Descripcion])
+     VALUES
+           (3, 'Violación de derechos')
+GO
+
+INSERT INTO [dbo].[MotivoDenuncia]
+           ([IdMotivoDenuncia]
+           ,[Descripcion])
+     VALUES
+           (4, 'Contiene información política')
+GO
